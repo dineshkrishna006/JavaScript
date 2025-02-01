95b123fdec8a8a2efccfc87ad7bc291cdb72856c
dineshkrishna006 <122519846+dineshkrishna006@users.noreply.github.com>
2025-02-01 04:50:06 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,1 +1,15 @@
-alert(4+3);
+
+let name = 'Dinesh'
+let age =20
+let big_number = BigInt(123333)
+let employeed = false
+let salary
+let company = null
+
+console.table([typeof name,typeof age,typeof big_number,typeof employeed,typeof salary,typeof company]);
+
+const person ={
+	name:'Dinesh',
+	age:20,
+}
+console.log(typeof person);
