72d21e83e7057d4048e918519f743d3b8e4788e0
dineshkrishna006 <122519846+dineshkrishna006@users.noreply.github.com>
2025-02-01 22:57:32 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,25 @@
+
+// const s = 'DiNesH';
+// console.log(s.toUpperCase())//DINESH
+// console.log(s.toLowerCase())//dinesh
+// console.log(s.indexOf('N'))//2
+// console.log(s.indexOf('a'))//-1
+// console.log(s.includes('i'))//true
+// console.log(s.includes('d'))//false
+// console.log(s.startsWith('Di'))
+
+const str = 'abc-def-gahi';
+console.log(str.endsWith('i'));//true
+console.log(str.length)//12
+console.log(str.split('-'))//['abc','def','gahi']
+console.log(str.slice(0,5));//abc-d
+console.log(str.slice(4));//def-gahi
+console.log(str[0])//a
+console.log(str.charAt(0))//a
+console.log(str.charCodeAt(0))//97
+
+// const trimEx = '  abcd  '
+// console.log(trimEx.trim())//abcd removes white spaces from front and back
+// console.log(str.replace('a','b'))//replaces the first a with b
+// console.log(str.replaceAll('a','b'))//replaces all the occurnces of a with b
+
