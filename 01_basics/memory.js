d77eed451ab262e131483abe853e30fb5e939454
dineshkrishna006 <122519846+dineshkrishna006@users.noreply.github.com>
2025-02-01 22:02:16 +0000

    memory-stack&heap

---
+++ 01_basics/memory.js
@@ -0,0 +1,16 @@
+
+// let name_ = 'Dinesh Krishna';
+// let name_copy = name_;
+// console.table([name_,name_copy])
+// name_copy='Krishna'
+// console.table([name_,name_copy])
+
+
+// const arr =[1,2,3,'a','b','c'];
+// const arr_copy = arr;
+// console.table([arr,arr_copy])
+// arr[0]='changed through arr[0]'
+// console.table([arr[0],arr_copy[0]])
+// arr_copy[0]='changed through arr_copy and its affecting arr';
+// console.table([arr,arr_copy])
+
--- 01_basics/summary_datatypes.js
+++ 01_basics/summary_datatypes.js
@@ -1,15 +1,22 @@
-const arr =[1,2,'abc']
-console.log(arr);
-console.log(typeof arr)
+// const arr =[1,2,'abc']
+// console.log(arr);
+// console.log(typeof arr)
 
 const myfunc = function square(){
     console.log('Funcation call');
 }
 console.log(typeof myfunc)
 
-const my_object ={
-    name:'Dinesh',
-    age:20,
+// const my_object ={
+//     name:'Dinesh',
+//     age:20,
+// }
+// console.log(my_object)
+// console.log(typeof my_object)
+
+function abc(){
+    console.log("abc function");
+    
 }
-console.log(my_object)
-console.log(typeof my_object)
+
+console.log(typeof abc);
