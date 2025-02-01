89715b0646cc1f8fd00b44512ae73c9f5081374b
dineshkrishna006 <122519846+dineshkrishna006@users.noreply.github.com>
2025-02-01 20:01:52 +0000

    summary of datatypes

---
+++ 01_basics/summary_datatypes.js
@@ -0,0 +1,15 @@
+const arr =[1,2,'abc']
+console.log(arr);
+console.log(typeof arr)
+
+const myfunc = function square(){
+    console.log('Funcation call');
+}
+console.log(typeof myfunc)
+
+const my_object ={
+    name:'Dinesh',
+    age:20,
+}
+console.log(my_object)
+console.log(typeof my_object)
